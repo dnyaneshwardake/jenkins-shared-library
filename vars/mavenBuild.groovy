7051c45424604373cb3d464b340aa7d67708457e
Dnyaneshwar Dake <dnyaneshwar3d@gmail.com>
2022-04-29 00:24:34 -0500

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -7,19 +7,10 @@ def call(){
     stages { 
         stage('Build') { 
             steps {
-                sh 'mvn -B -DskipTests clean package' 
+                sh 'mvn -B -DskipTests clean install' 
             }
         }
-        stage('Test') {
-            steps {
-                sh 'mvn test'
-            }
-            post {
-                always {
-                    junit 'target/surefire-reports/*.xml'
-                }
-            }
-        }
+        
         stage('Deliver') {
             steps {
                 sh './jenkins/scripts/deliver.sh'
