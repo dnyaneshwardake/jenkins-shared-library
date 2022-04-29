1d498fc83476687ad9ea0677cd9ab539f7908bb9
Dnyaneshwar Dake <dnyaneshwar3d@gmail.com>
2022-04-29 13:25:57 -0500

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -4,11 +4,14 @@ def call(){
     registry = "docker_hub_account/repository_name"
     registryCredential = 'dockerhub'
     }
+    
+    agent any
+    
     stages {
     stage('Building image') {
       steps{
         script {
-          docker.build registry + ":$BUILD_NUMBER"
+            docker.build registry + ":$BUILD_NUMBER"
           }
         }
       }
