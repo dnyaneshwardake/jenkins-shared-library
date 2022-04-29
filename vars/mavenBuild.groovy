c5fca0af1c49226bfcbc4c15cca95cbb07c2450b
Dnyaneshwar Dake <dnyaneshwar3d@gmail.com>
2022-04-28 23:59:48 -0500

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,20 +1,8 @@
 def call(){
   pipeline {
     agent any
-    tools {
-        maven 'Maven 3.3.9'
-        jdk 'jdk17'
-    }
-    stages {
-      stage ('Initialize') {
-            steps {
-                sh '''
-                    echo "PATH = ${PATH}"
-                    echo "M2_HOME = ${M2_HOME}"
-                '''
-            }
-        }
-      
+
+    stages { 
         stage('Build') { 
             steps {
                 sh 'mvn -B -DskipTests clean package' 
