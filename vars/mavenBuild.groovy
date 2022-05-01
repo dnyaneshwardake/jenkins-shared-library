41abc477903efe021b03f991879f8c5fa8c023e3
Dnyaneshwar Dake <dnyaneshwar3d@gmail.com>
2022-05-01 13:55:05 -0500

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -34,7 +34,11 @@ def call(){
             }
         }
       }
-      
+      stage('Cleaning up') {
+        steps{
+          sh "docker rmi $registry:$BUILD_NUMBER"
+        }
+      }
     }
   }
 }
